e2483781cf62c9713403623bf3e05feef27126f6
YoavPaz <150516622+YoavPaz@users.noreply.github.com>
2026-06-01 08:51:30 +0300

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,14 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include "main.h"
+
+class functions{
+    public:
+    functions() {};
+    static void G1Move(ParamValue* params, int count);
+    static void G0Move(ParamValue* params, int count);
+
+};
+
+#endif
